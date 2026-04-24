6fda34b788b52740621a43599b1a7d0c2ec1a147
gbhemalatha <162114706+gbhemalatha@users.noreply.github.com>
2026-04-24 14:05:27 +0530

    Add files via upload

--- add.py
+++ add.py
@@ -1,2 +1,10 @@
-name = input("Enter your name: ")
-print("Hello", name)
+import streamlit as st
+
+st.title("Simple Python App")
+
+name = st.text_input("Enter your name:")
+age = st.number_input("Enter your age:", step=1)
+
+if st.button("Submit"):
+    st.write("Hello", name)
+    st.write("You are", age, "years old")
